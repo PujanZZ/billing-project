3802b8e2d3f38017dc1682a8c8a9632d7ba784bb
PujanZZ <pujan2001@gmail.com>
2024-03-07 08:47:32 +0530

    auth fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SessionExpiredComponent } from './shared/components/session-expired/session-expired.component';
 import { AccessDeniedComponent } from './shared/components/access-denied/access-denied.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { AuthGuard } from './shared/guards/auth.guard';
+import { SecureInnerPagesGuard } from './shared/guards/secure-inner-pages.guard';
 
 /** Main Parent Routes*/
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'auth', canActivate: [], loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'admin', canActivate: [], loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  { path: 'auth', canActivate: [SecureInnerPagesGuard], loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'admin', canActivate: [AuthGuard], loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
   { path: 'session-expired', component: SessionExpiredComponent },
   { path: 'access-denied', component: AccessDeniedComponent },
   { path: '**', pathMatch: 'full', component: NotFoundComponent },
--- src/app/shared/guards/secure-inner-pages.guard.ts
+++ src/app/shared/guards/secure-inner-pages.guard.ts
@@ -15,7 +15,7 @@ export class SecureInnerPagesGuard {
     // Check if the logged-in user is not empty, null, or undefined
     if (!this.utilsService.isEmptyObjectOrNullUndefined(this.utilsService.getLoggedInUser())) {
       // Redirect to the customer dashboard to prevent access to the login page after login
-      this.router.navigate(['/customer/dashboard']);
+      this.router.navigate(['/admin/dashboard']);
     }
     // Allow the route activation
     return true;
